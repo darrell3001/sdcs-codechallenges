c05e21d95ff3b0b23c713fe366eff86a418ef11b
Darrell Sturdivant <darrell3001@gmail.com>
2019-10-27 13:19:03 -0700

    refactored solve.js

--- solve.js
+++ solve.js
@@ -1,48 +1,36 @@
 function solve(st, a, b) {
-  var n = "";
-  var r = "";
+  if ((st.length = 0)) return st;
+  if (a > b) return st;
+  if (a > st.length) return st;
+  if (a < 1) return st;
 
-  console.log(`str = ${st}, a = ${a}, b = ${b}`);
+  var f = st.slice(0, a < st.length ? a : st.length);
+  var l = st.slice(b < st.length ? b : st.length);
+  var m = st.slice(a, b);
 
-  var myArray = st.split("");
+  // console.log(`str = ${st}, a = ${a}, b = ${b}`);
+  // console.log(`f = ${f}, l = ${l}, m = ${m}`);
+  // console.log(`st.length = ${st.length}`);
 
-  for (j = b; j >= a; j--) {
-    if (j < myArray.length) {
-      r += myArray[j];
-      // console.log(`r = ${r}, j = ${j}, myArray[j] = ${myArray[j]}`);
-    }
-  }
+  var r = m
+    .split("")
+    .reverse()
+    .join("");
 
-  console.log(`st.length = ${st.length}`);
-  console.log("");
+  var n = f + r + l;
 
-  var f = "";
-  var l = "";
-
-  for (i = 0; i < st.length; i++) {
-    // console.log(`n = ${n}, i = ${i}, myArray[i] = ${myArray[i]}`);
-
-    // on the "c"
-    if (i < a && i < b) {
-      f += myArray[i];
-    }
-
-    // in the rev part
-    // if (i >= a && i <= b) {
-    //   n += r;
-    // }
-
-    //
-    if (i > a && i > b) {
-      l += myArray[i];
-    }
-  }
-
-  n = f + r + l;
-
-  console.log(`n = ${n}`);
+  // console.log(`n = ${n}`);
   return n;
 }
 
 //  ,"cawedors");
-solve("codingIsFun", 2, 15);
+// solve("codingIsFun", 2, 10);
+console.log(solve("abcdefghijkl", 52, 100));
+console.log(solve("abcdefghijkl", 1, 100));
+console.log(solve("abcdefghijkl", 100, 52));
+console.log(solve("abcdefghijkl", 0, 100));
+console.log(solve("abcdefghijkl", -1, 100));
+console.log(solve("abcdefghijkl", -1, 2));
+console.log(solve("abcdefghijkl", -1, -2));
+console.log(solve("abcdefghijkl", 100, 2));
+console.log(solve("", 0, 0));
